1b9e8886315450ed310a9ceb4af04deb40d32832
Luiz E. Buzato <buzato@ic.unicamp.br>
2014-10-25 12:28:18 -0200

    HardwareTest is is place holder, implement it if necessary.

---
+++ src/test/java/engsoft/HardwareTest.java
@@ -0,0 +1,24 @@
+package engsoft;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HardwareTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Implemente aqui o teste para o novo Hardware, se necessario.");
+	}
+
+}
